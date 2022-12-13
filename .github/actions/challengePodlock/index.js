c592abdbf3c02db5f2a92870a0505a31fbd95eeb
Brayden Williams <brayden@zorpcorp.com>
2022-12-13 04:43:37 -0800

    Use tracked dependencies for actions

--- .github/actions/challengePodlock/index.js
+++ .github/actions/challengePodlock/index.js
@@ -1,9 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const challengePodlock = require("./src/challengePodlock.js");
+const index = require("./src/index.js");
 const config = JSON.parse(JSON.parse(core.getInput("pods")));
 
-if (!challengePodlock(config)) {
+if (!index(config)) {
     core.setFailed('Podfile is not synced.  Run `pod install` in the iOS directory and commit the new Podfile.lock')
 } else {
     console.log('Pods appear to be synced.')
